1fb3e78a87a9f6862d7578be1b0b97dcdc93a2aa
KONG1034 <kyh93054@naver.com>
2022-10-02 22:30:00 +0900

    Kong - 이미지&사이드메뉴&reset.css 수정

--- js/index.js
+++ js/index.js
@@ -1,25 +1,59 @@
-const h_m_link = document.querySelector('#h_m_link');
-let s_container = document.querySelector('.s_container');
 
-h_m_link.addEventListener('click', function() {
-    s_container.style.right = 0;
+$('#h_m_link').click(function() {
+    $('.s_container').show();
+    $('.sidebar_overlay').show();
+    if($('.s_container').attr('style').indexOf('block') !== -1) {
+        window.onscroll = function () { window.scrollTo(0, 0); };
+        $('.w_p_card_container').slick('slickPause');
+        $('.g_r_content_container').slick('slickPause');
+        $('.p_r_container').slick('slickPause');
+    }
 })
 
-h_m_link.addEventListener('blur', function() {
-    s_container.style.right = '-40%';
+$('#s_m_cancel').click(function() {
+    $('.s_container').hide();
+    $('.sidebar_overlay').hide();
+    $('.w_p_card_container').slick('slickPlay');
+    $('.g_r_content_container').slick('slickPlay');
+    $('.p_r_container').slick('slickPlay');
+    window.onscroll = function () { };
 })
 
 /* 메뉴 스크롤 이동 */
-let floatPosition = parseInt($(".s_container").css('top'));
-$(window).scroll(function() {
-    let scrollTop = $(window).scrollTop();
-    let newPosition = scrollTop + floatPosition + "px";
+// let bodyPosition = document.body.scrollHeight;
+// let floatPosition = parseInt($(".s_container").css('top'));
+// $(window).scroll(function() {
+//     let scrollTop = $(this).scrollTop();
+//     console.log(scrollTop)
+    
+//     if ( scrollTop < 100 ) {
+//         scrollTop = 0;
+//     }
+//     else if ( scrollTop > 5600 ) {
+//         scrollTop = 5600;
+//     }
 
-    $("#floatMenu").css('top', newPosition);
-    $(".s_container").stop().animate({
-        "top" : newPosition
-    }, 100);
-}).scroll();
+//     $('.s_container').stop().animate({top:scrollTop}, 100);
+//     // let scrollTop = $(window).scrollTop();
+//     // let newPosition = scrollTop + floatPosition + "px";
+
+//     // console.log(bodyPosition)
+//     // console.log('float => '+floatPosition)
+//     // console.log('scrollTop => '+scrollTop);
+//     // if(scrollTop < 4085.5) {
+//     //     $('.s_container').on('scroll touchmove mousewheel', function(event) {
+//     //         event.preventDefault();
+//     //         event.stopPropagation();
+//     //         return false;
+//     //       });
+//     // } else {
+//     //     $('.s_container').off('scroll touchmove mousewheel');
+//     //     $("#floatMenu").css('top', newPosition);
+//     //     $(".s_container").stop().animate({
+//     //         "top" : newPosition
+//     //     }, 100);
+//     // }
+// }).scroll();
 
 /* 이미지 슬라이더 */
 $('.w_p_card_container').slick({
@@ -63,7 +97,7 @@ $('.g_r_content_container').slick({
 						breakpoint: 1024, //화면 사이즈 768px
 						settings: {	
 							//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-							slidesToShow:3,
+							slidesToShow:2,
                             slidesToScroll: 1
 						} 
 					},
